ad85e4ade4664244267417c7725e16a2b5fa1e45
Kevin Hahn <hahn.kev@gmail.com>
2017-11-25 13:14:12 -0800

    add entity service to help with crud

---
+++ Backend/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using LinqToDB.Mapping;
+
+namespace Backend.Entities
+{
+    public abstract class BaseEntity
+    {
+        [PrimaryKey]
+        public Guid Id { get; set; } = Guid.Empty;
+    }
+}
---
+++ Backend/Services/EntityService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Backend.DataLayer;
+using Backend.Entities;
+using LinqToDB;
+
+namespace Backend.Services
+{
+    public class EntityService
+    {
+        private readonly DbConnection _dbConnection;
+
+        public EntityService(DbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public void Save<T>(T entity) where T : BaseEntity
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+            if (entity.Id == Guid.Empty)
+            {
+                entity.Id = Guid.NewGuid();
+                _dbConnection.Insert(entity);
+                return;
+            }
+            if (_dbConnection.Update(entity) != 1)
+                throw new Exception($"{typeof(T).Name} not found for id: {entity.Id}");
+        }
+    }
+}
