51d7387018816177e995e8b2a8c329dfb55a4848
Kevin Hahn <hahn.kev@gmail.com>
2017-12-07 16:15:28 -0800

    allow editing org group supervisor approver value

--- Frontend/src/app/people/groups/org-group.ts
+++ Frontend/src/app/people/groups/org-group.ts
@@ -1,9 +1,10 @@
 export class OrgGroup {
 
   constructor(public groupName?: string,
-              public type?: string,
-              public supervisor?: string,
-              public parentId?: string,
-              public id?: string) {
+    public type?: string,
+    public supervisor?: string,
+    public parentId?: string,
+    public id?: string,
+    public approverIsSupervisor?: boolean) {
   }
 }
