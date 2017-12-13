18775ee2c48c13317b661ebcc49078d7cd04185c
Kevin Hahn <hahn.kev@gmail.com>
2017-12-12 21:52:11 -0800

    add staff training class

---
+++ Backend/Entities/StaffTraining.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Backend.Entities
+{
+    public class StaffTraining: BaseEntity
+    {
+        public Guid StaffId { get; set; }
+        public Guid TrainingRequirementId { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
