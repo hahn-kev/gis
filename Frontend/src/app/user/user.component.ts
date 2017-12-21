ffcb1cad140afdf02cac05c39b577f9e3abf82e9
Kevin Hahn <hahn.kev@gmail.com>
2017-12-20 17:33:35 -0800

    add person field to user editor

--- Frontend/src/app/user/user.component.ts
+++ Frontend/src/app/user/user.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from './user.service';
 import { MatDialog } from '@angular/material';
 import { ConfirmDialogComponent } from '../dialog/confirm-dialog/confirm-dialog.component';
+import { PersonService } from '../people/person.service';
+import { Observable } from 'rxjs/Observable';
+import { Person } from '../people/person';
 
 @Component({
   selector: 'app-user',
@@ -16,14 +19,17 @@ export class UserComponent implements OnInit {
   public isSelf: boolean;
   public password: string;
   public errorMessage: string;
+  public people: Observable<Person[]>;
 
   constructor(private route: ActivatedRoute,
               private userService: UserService,
               private router: Router,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              private personService: PersonService) {
+    this.people = this.personService.getAll();
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.data.subscribe((data: { user: User, isNew: boolean, isSelf: boolean }) => {
       this.user = data.user;
       this.isNew = data.isNew;
@@ -32,22 +38,22 @@ export class UserComponent implements OnInit {
   }
 
 
-  async saveUser() {
+  async saveUser(): Promise<void> {
     await this.userService.saveUser(this.user, this.password, this.isNew, this.isSelf);
     this.router.navigate([this.isSelf ? '/home' : '/user/admin']);
   }
 
-  async grantAdmin() {
+  async grantAdmin(): Promise<void> {
     await this.userService.grantAdmin(this.user.userName);
     this.user.isAdmin = true;
   }
 
-  async revokeAdmin() {
+  async revokeAdmin(): Promise<void> {
     await this.userService.revokeAdmin(this.user.userName);
     this.user.isAdmin = false;
   }
 
-  deleteUser() {
+  deleteUser(): void {
     const dialogRef = this.dialog.open(ConfirmDialogComponent,
       {data: ConfirmDialogComponent.Options(`Delete User ${this.user.userName}?`, 'Delete', 'Cancel')});
     dialogRef.afterClosed().subscribe(async result => {
