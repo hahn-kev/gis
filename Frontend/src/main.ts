7488acf99ef9d81459fd987d822e30280cd8c7dc
Kevin Hahn <hahn.kev@gmail.com>
2018-02-28 14:55:13 -0800

    enable hot module reload

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
 import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('Ammm.. HMR is not enabled for webpack');
+  }
+} else {
+  bootstrap();
+}
