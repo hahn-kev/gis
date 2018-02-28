3303dc863a97976000f1cb46fac1bfe17d8837d3
Kevin Hahn <hahn.kev@gmail.com>
2018-02-27 17:08:55 -0800

    fix issue where loading bar wouldn't go away on leave request page

--- Frontend/src/app/people/leave-request/people-with-leave-resolver.service.ts
+++ Frontend/src/app/people/leave-request/people-with-leave-resolver.service.ts
@@ -4,22 +4,15 @@ import { PersonAndLeaveDetails } from './person-and-leave-details';
 import { Observable } from 'rxjs/Observable';
 import { LeaveRequestService } from './leave-request.service';
 import { LoginService } from '../../services/auth/login.service';
-import { first, switchMap, tap } from 'rxjs/operators';
+import { first, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class PeopleWithLeaveResolverService implements Resolve<PersonAndLeaveDetails[]> {
   resolve(route: ActivatedRouteSnapshot,
           state: RouterStateSnapshot): Observable<PersonAndLeaveDetails[]> | Promise<PersonAndLeaveDetails[]> | PersonAndLeaveDetails[] {
-    return this.loginService.hasAnyRole(['admin', 'hr']).pipe(
-      switchMap(isHrAdmin => this.leaveRequestService.listPeopleWithLeave(isHrAdmin)),
-      first(),
-      tap(x => {
-        },
-        e => {
-        },
-        () => console.log('done'))
-    );
-    //todo complete isn't always called, no idea why documentation website is down... help!
+    return this.loginService.hasAnyRole(['admin', 'hr'])
+      .pipe(first(),
+        switchMap(isHrAdmin => this.leaveRequestService.listPeopleWithLeave(isHrAdmin)));
   }
 
   constructor(private leaveRequestService: LeaveRequestService, private loginService: LoginService) {
