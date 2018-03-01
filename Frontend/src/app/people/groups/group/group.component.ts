9c1f7a00ead82409d6b3a6e7e137f5cca106068e
Kevin Hahn <hahn.kev@gmail.com>
2018-02-28 23:38:07 -0800

    add save delete confirmation to groups and training requirements

--- Frontend/src/app/people/groups/group/group.component.ts
+++ Frontend/src/app/people/groups/group/group.component.ts
@@ -3,7 +3,8 @@ import { OrgGroup } from '../org-group';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GroupService } from '../group.service';
 import { Person } from '../../person';
-import { LinkType, OrgChain } from '../org-chain';
+import { OrgChain } from '../org-chain';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-group',
@@ -11,20 +12,22 @@ import { LinkType, OrgChain } from '../org-chain';
   styleUrls: ['./group.component.scss']
 })
 export class GroupComponent implements OnInit {
-  LinkType = LinkType;
   public group: OrgGroup;
   public people: Person[];
   public groups: OrgGroup[];
   public orgChain: OrgChain;
+  private isNew = false;
 
   constructor(private route: ActivatedRoute,
-    private router: Router,
-    private groupService: GroupService) {
+              private router: Router,
+              private groupService: GroupService,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
     this.route.data.subscribe((value) => {
       this.group = value.group;
+      this.isNew = !this.group.id;
       this.groups = value.groups;
       this.people = value.people;
       setTimeout(() => this.refreshOrgChain());
@@ -37,6 +40,7 @@ export class GroupComponent implements OnInit {
 
   async save(): Promise<void> {
     await this.groupService.updateGroup(this.group);
-    this.router.navigate(['/groups']);
+    this.router.navigate(['/groups/list']);
+    this.snackBar.open(`${this.group.type} ${this.isNew ? 'Added' : 'Saved'}`, null, {duration: 2000});
   }
 }
--- Frontend/src/app/people/training-requirement/training-edit/training-edit.component.ts
+++ Frontend/src/app/people/training-requirement/training-edit/training-edit.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { TrainingRequirementService } from '../training-requirement.service';
 import { TrainingRequirement } from '../training-requirement';
 import { Year } from '../year';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { ConfirmDialogComponent } from '../../../dialog/confirm-dialog/confirm-dialog.component';
 
 @Component({
@@ -14,16 +14,19 @@ import { ConfirmDialogComponent } from '../../../dialog/confirm-dialog/confirm-d
 export class TrainingEditComponent implements OnInit {
   public training: TrainingRequirement;
   public years: Year[];
+  public isNew = false;
 
   constructor(private route: ActivatedRoute,
               private trainingService: TrainingRequirementService,
               private router: Router,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
     this.route.data.subscribe((value: { training: TrainingRequirement }) => {
       this.training = value.training;
+      this.isNew = !this.training.id;
       if (!this.training.id) {
         this.training.firstYear = new Date().getUTCFullYear();
         this.training.scope = 'AllStaff';
@@ -34,20 +37,22 @@ export class TrainingEditComponent implements OnInit {
 
   async save(): Promise<void> {
     await this.trainingService.save(this.training);
-    this.router.navigate(['../..', 'list'], {relativeTo: this.route});
+    this.router.navigate(['training', 'list']);
+    this.snackBar.open(`Training Requirement ${this.isNew ? 'Added' : 'Saved'}`, null, {duration: 2000});
   }
 
   async delete() {
     const dialogRef = this.dialog.open(ConfirmDialogComponent,
       {
-        data: ConfirmDialogComponent.Options(`Deleting Training requirement, data will be lost, this can not be undone`,
+        data: ConfirmDialogComponent.Options(`Deleting Training requirement, any training records will also be deleted`,
           'Delete',
           'Cancel')
       });
     const result = await dialogRef.afterClosed().toPromise();
     if (!result) return;
     await this.trainingService.deleteRequirement(this.training.id);
-    this.router.navigate(['../..', 'list'], {relativeTo: this.route});
+    this.router.navigate(['training', 'list'], {relativeTo: this.route});
+    this.snackBar.open(`Training Requirement Deleted`, null, {duration: 2000});
   }
 
 }
