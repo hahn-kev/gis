40dfe6d9c3d6776d16c04e026c13ff689d6b1bd7
Kevin Hahn <hahn.kev@gmail.com>
2018-03-01 00:08:04 -0800

    move polyfill to it's correct location

--- Frontend/src/polyfills.ts
+++ Frontend/src/polyfills.ts
@@ -40,6 +40,14 @@
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+import 'assets/js/css-polyfills.js'
 
 
 /**
@@ -47,20 +55,6 @@ import 'core-js/es7/reflect';
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-
-
-/***************************************************************************************************
- * Zone JS is required by Angular itself.
- */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
 /**
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
