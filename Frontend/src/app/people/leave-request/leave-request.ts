d996bc6b8fc1b11616d63f69167ec3209e1ce974
Kevin Hahn <hahn.kev@gmail.com>
2018-03-26 13:24:15 +0700

    display approver on leave request page, also hide vacation chip for non thai

--- Frontend/src/app/people/leave-request/leave-request.ts
+++ Frontend/src/app/people/leave-request/leave-request.ts
@@ -1,18 +1,18 @@
-export class LeaveRequest {
+import { BaseEntity } from '../../classes/base-entity';
 
-  constructor(public id?: string,
-    public personId?: string,
-    public startDate?: Date,
-    public endDate?: Date,
-    public type?: string,
-    public approved?: boolean,
-    public createdDate?: Date) {
-  }
-
+export class LeaveRequest extends BaseEntity {
+  public personId: string;
+  public startDate: Date;
+  public endDate: Date;
+  public type: string;
+  public approved: boolean;
+  public approvedById: string;
+  public createdDate: Date;
   public reason: string;
   public days: number;
   public overrideDays: boolean;
 }
+
 
 export class LeaveRequestWithNames extends LeaveRequest {
   public requesterName: string;
