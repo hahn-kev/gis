5fd7e004babc9a683be1d3504157da5e6f3abfb6
Kevin Hahn <hahn.kev@gmail.com>
2018-03-26 14:27:39 +0700

    implement custom sorting on data source

--- Frontend/src/app/classes/app-data-source.ts
+++ Frontend/src/app/classes/app-data-source.ts
@@ -3,10 +3,29 @@ import { ActivatedRoute } from '@angular/router';
 import { MatTableDataSource } from '@angular/material';
 
 export class AppDataSource<T> extends MatTableDataSource<T> {
-  public ObserverData = new BehaviorSubject<T[]>([]);
+  private customColumnAccessors: { [key: string]: (data: T) => string | number } = {};
 
   constructor() {
     super();
+    let ogSort = this.sortingDataAccessor;
+    this.sortingDataAccessor = (data, sortHeaderId) => {
+      if (this.customColumnAccessors[sortHeaderId]) return this.customColumnAccessors[sortHeaderId](data);
+      let index = 0;
+      let paramIndex = sortHeaderId.indexOf('.', index);
+      while (paramIndex > -1) {
+        data = data[sortHeaderId.substring(index, paramIndex)];
+        index = paramIndex + 1;
+        paramIndex = sortHeaderId.indexOf('.', index);
+      }
+      if (index > 0) {
+        sortHeaderId = sortHeaderId.substring(index);
+      }
+      return ogSort(data, sortHeaderId);
+    };
+  }
+
+  customColumnAccessor(columnId: string, accessor: (data: T) => string | number) {
+    this.customColumnAccessors[columnId] = accessor;
   }
 
   bindToRouteData(route: ActivatedRoute, dataName: string): void {
