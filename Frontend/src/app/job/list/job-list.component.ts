13f6754da30c2ccf30eb8ebae4ae427ab1044dba
Kevin Hahn <hahn.kev@gmail.com>
2018-04-17 13:39:44 +0700

    job list: add filter to job list based on type, persist filters in url

--- Frontend/src/app/job/list/job-list.component.ts
+++ Frontend/src/app/job/list/job-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppDataSource } from '../../classes/app-data-source';
-import { ActivatedRoute } from '@angular/router';
-import { Job, JobWithFilledInfo } from '../job';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Job, JobType, JobWithFilledInfo } from '../job';
 import { MatSort } from '@angular/material';
 
 @Component({
@@ -10,29 +10,69 @@ import { MatSort } from '@angular/material';
   styleUrls: ['./job-list.component.scss']
 })
 export class JobListComponent implements OnInit {
+  public jobTypes = Object.keys(JobType);
   public dataSource: AppDataSource<JobWithFilledInfo>;
   public typeName = Job.typeName;
   public showOnlyOpen = false;
+  public shownTypes: JobType[] = [];
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
+
   }
 
   ngOnInit(): void {
     this.dataSource = new AppDataSource<JobWithFilledInfo>();
     this.dataSource.sort = this.sort;
     this.dataSource.bindToRouteData(this.route, 'jobs');
-    this.dataSource.customFilter = row => this.showOnlyOpen ? row.open > 0 : true;
+    this.dataSource.customFilter = (row: JobWithFilledInfo) => {
+      if (this.showOnlyOpen && row.open <= 0) return false;
+      if (!this.shownTypes.includes(row.type)) return false;
+      return true;
+    };
     this.dataSource.filterPredicate = ((data, filter) =>
-      (data.title || '').toUpperCase().includes(filter)
-      || (data.jobDescription || '').toUpperCase().includes(filter)
-      || (data.orgGroupName || '').toUpperCase().includes(filter)
+        (data.title || '').toUpperCase().includes(filter)
+        || (data.jobDescription || '').toUpperCase().includes(filter)
+        || (data.orgGroupName || '').toUpperCase().includes(filter)
     );
+    this.route.queryParamMap.subscribe(params => {
+      this.showOnlyOpen = params.has('onlyOpen') ? params.get('onlyOpen') == 'true' : false;
+      this.shownTypes = (params.has('types') ? params.getAll('types') : this.jobTypes)
+        .map(jt => JobType[jt]);
+      let oldFilter = this.dataSource.filter;
+      this.dataSource.filter = (params.get('filter') || '').toUpperCase();
+      this.dataSource.filterUpdated();
+    });
   }
 
 
   applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toUpperCase();
+    this.setQuery(filterValue, this.showOnlyOpen, this.shownTypes);
   }
 
+  applyShowOnlyOpen(onlyOpen: boolean) {
+    this.setQuery(this.dataSource.filter, onlyOpen, this.shownTypes);
+  }
+
+  applyShownTypes(types: JobType[]) {
+    this.setQuery(this.dataSource.filter, this.showOnlyOpen, types);
+  }
+
+  setQuery(filter: string, onlyOpen: boolean, types: JobType[]) {
+    let params: Params = {
+      onlyOpen: onlyOpen,
+      types: types
+    };
+    if (filter !== '') params.filter = filter;
+    this.router.navigate(['.'], {
+      relativeTo: this.route,
+      queryParams: params
+    });
+  }
+
+  jobSelectLabel(types: JobType[]) {
+    if (types.length == this.jobTypes.length) return 'All';
+    if (!types.includes(JobType.SchoolAid) && types.length + 1 == this.jobTypes.length) return 'Staff Jobs';
+    return types.join(', ');
+  }
 }
