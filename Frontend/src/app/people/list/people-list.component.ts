c8548f0e8eb0e7772be6f67b93a60ec4b4333c0d
Kevin Hahn <hahn.kev@gmail.com>
2018-04-17 07:42:27 +0700

    person list: add sortable headers

--- Frontend/src/app/people/list/people-list.component.ts
+++ Frontend/src/app/people/list/people-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Person } from '../person';
 import { ActivatedRoute } from '@angular/router';
 import { AppDataSource } from '../../classes/app-data-source';
+import { MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-people-list',
@@ -10,12 +11,14 @@ import { AppDataSource } from '../../classes/app-data-source';
 })
 export class PeopleListComponent implements OnInit {
   public dataSource: AppDataSource<Person>;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.dataSource = new AppDataSource<Person>();
+    this.dataSource.sort = this.sort;
     this.dataSource.bindToRouteData(this.route, 'people');
     this.dataSource.filterPredicate = ((data, filter) =>
       data.firstName.toUpperCase().startsWith(filter) || data.lastName.toUpperCase().startsWith(filter));
