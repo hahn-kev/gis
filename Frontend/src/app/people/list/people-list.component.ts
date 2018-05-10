657a734bea95e63e1ce8549ea734ac8477e7d592
Kevin Hahn <hahn.kev@gmail.com>
2018-05-09 22:36:34 -0700

    person list: preserve search term in url

--- Frontend/src/app/people/list/people-list.component.ts
+++ Frontend/src/app/people/list/people-list.component.ts
@@ -3,24 +3,28 @@ import { Person } from '../person';
 import { ActivatedRoute } from '@angular/router';
 import { AppDataSource } from '../../classes/app-data-source';
 import { MatSort } from '@angular/material';
+import { UrlBindingService } from '../../services/url-binding.service';
 
 @Component({
   selector: 'app-people-list',
   templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.scss']
+  styleUrls: ['./people-list.component.scss'],
+  providers: [UrlBindingService]
 })
 export class PeopleListComponent implements OnInit {
   public dataSource: AppDataSource<Person>;
   @ViewChild(MatSort) sort: MatSort;
+
   get title() {
     return this.route.snapshot.data.title || 'People';
   }
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, public urlBinding: UrlBindingService<{ search: string }>) {
   }
 
   ngOnInit(): void {
     this.dataSource = new AppDataSource<Person>();
+    this.urlBinding.addParam('search', '').subscribe(value => this.dataSource.filter = value.toUpperCase());
     this.dataSource.sort = this.sort;
     this.dataSource.bindToRouteData(this.route, 'people');
     this.dataSource.filterPredicate = ((data, filter) =>
