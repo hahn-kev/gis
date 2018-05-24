fd9a4bc9263086a4e2eee0cc5a6620e702366a22
Kevin Hahn <hahn.kev@gmail.com>
2018-05-23 18:03:56 -0700

    sending org list: preserve search in url

--- Frontend/src/app/mission-org/list/mission-org-list.component.ts
+++ Frontend/src/app/mission-org/list/mission-org-list.component.ts
@@ -3,28 +3,26 @@ import { AppDataSource } from '../../classes/app-data-source';
 import { ActivatedRoute } from '@angular/router';
 import { MissionOrgWithNames } from '../mission-org';
 import { MatSort } from '@angular/material';
+import { UrlBindingService } from '../../services/url-binding.service';
 
 @Component({
   selector: 'app-mission-org-list',
   templateUrl: './mission-org-list.component.html',
-  styleUrls: ['./mission-org-list.component.scss']
+  styleUrls: ['./mission-org-list.component.scss'],
+  providers: [UrlBindingService]
 })
 export class MissionOrgListComponent implements OnInit {
-  public dataSource: AppDataSource<MissionOrgWithNames>;
+  public dataSource = new AppDataSource<MissionOrgWithNames>();
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, public urlBinding: UrlBindingService<{ search: string }>) {
+    this.urlBinding.addParam('search', '').subscribe(value => this.dataSource.filter = value.trim().toUpperCase());
   }
 
   ngOnInit(): void {
-    this.dataSource = new AppDataSource<MissionOrgWithNames>();
     this.dataSource.sort = this.sort;
     this.dataSource.bindToRouteData(this.route, 'missionOrgs');
-    this.dataSource.filterPredicate = ((data, filter) => data.name.toUpperCase().startsWith(filter));
-  }
-
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toUpperCase();
+    this.dataSource.filterPredicate = ((data, filter) => data.name.toUpperCase().includes(filter));
+    this.urlBinding.loadFromParams();
   }
 }
