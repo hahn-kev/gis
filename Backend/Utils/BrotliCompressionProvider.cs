a4b6f9e14846b7d9512800322614d5c7229f719a
Kevin Hahn <hahn.kev@gmail.com>
2018-07-02 11:27:36 -0700

    backend: switch to fast brotli compression

--- Backend/Utils/BrotliCompressionProvider.cs
+++ Backend/Utils/BrotliCompressionProvider.cs
@@ -6,9 +6,16 @@ namespace Backend.Utils
 {
     public class BrotliCompressionProvider : ICompressionProvider
     {
+        private readonly CompressionLevel _compressionLevel;
+        
+        public BrotliCompressionProvider(CompressionLevel compressionLevel = CompressionLevel.Fastest)
+        {
+            _compressionLevel = compressionLevel;
+        }
+
         public Stream CreateStream(Stream outputStream)
         {
-            return new BrotliStream(outputStream, CompressionLevel.Optimal, true);
+            return new BrotliStream(outputStream, _compressionLevel, true);
         }
 
         public string EncodingName => "br";
