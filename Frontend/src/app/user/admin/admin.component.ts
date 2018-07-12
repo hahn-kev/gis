166dd6e3d57b6310280e916a7bb0ea13fe2955ab
Kevin Hahn <hahn.kev@gmail.com>
2018-07-11 21:36:19 -0700

    fronted: fix sourcemaps build

--- Frontend/src/app/user/admin/admin.component.ts
+++ Frontend/src/app/user/admin/admin.component.ts
@@ -6,7 +6,7 @@ import { AppDataSource } from '../../classes/app-data-source';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
   dataSource: AppDataSource<User>;
