ba802a4c01e33a48fd5aff41d62b26a78c8bd07e
Kevin Hahn <hahn.kev@gmail.com>
2018-08-29 06:59:43 +0000

    leave list: add filters

--- Frontend/src/app/people/leave-request/leave-list/leave-list.component.ts
+++ Frontend/src/app/people/leave-request/leave-list/leave-list.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { LeaveRequest } from '../leave-request';
-import { AppDataSource } from 'app/classes/app-data-source';
-import { LoginService } from '../../../services/auth/login.service';
-import { LeaveTypeName } from '../../self/self';
-import { MatSort } from '@angular/material';
-import { Year } from '../../training-requirement/year';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {LeaveRequestWithNames} from '../leave-request';
+import {AppDataSource} from 'app/classes/app-data-source';
+import {LoginService} from '../../../services/auth/login.service';
+import {LeaveTypeName} from '../../self/self';
+import {MatSort} from '@angular/material';
+import {Year} from '../../training-requirement/year';
+import {UrlBindingService} from "../../../services/url-binding.service";
 
 @Component({
   selector: 'app-leave-list',
   templateUrl: './leave-list.component.html',
-  styleUrls: ['./leave-list.component.scss']
+  styleUrls: ['./leave-list.component.scss'],
+  providers: [UrlBindingService]
 })
 export class LeaveListComponent implements OnInit {
-  public dataSource: AppDataSource<LeaveRequest>;
+  public dataSource = new AppDataSource<LeaveRequestWithNames>();
   public yearNameFromDate = Year.schoolYearNameFromDate;
   public typeName = LeaveTypeName;
   public filteredByUser: string | null;
   public showingMine: boolean;
+  public title: string;
   public hrColumns = [
     'requester',
     'type',
@@ -41,17 +44,30 @@ export class LeaveListComponent implements OnInit {
   ];
   @ViewChild(MatSort) matSort: MatSort;
 
-  constructor(private route: ActivatedRoute, public loginService: LoginService) {
+  constructor(private route: ActivatedRoute, public loginService: LoginService,
+              private urlBinding: UrlBindingService<{ search: string, showApproved: boolean }>) {
     this.route.params.subscribe(value => {
       this.filteredByUser = value['personId'];
-      this.showingMine = this.filteredByUser === 'mine';
     });
+    this.route.url.subscribe(url => {
+      this.showingMine = url.some(value => value.path === 'mine');
+    });
+
+    this.urlBinding.addParam("search", '')
+      .subscribe(value => this.dataSource.filter = value.toUpperCase());
+    this.urlBinding.addParam("showApproved", false);
+    this.urlBinding.onParamsUpdated = values => this.dataSource.filterUpdated();
+    this.dataSource.customFilter = value => {
+      if (!this.urlBinding.values.showApproved && value.approved) return false;
+      return true;
+    };
+    this.dataSource.bindToRouteData(this.route, 'leave');
+    this.dataSource.filterPredicate = (data, filter) => data.requesterName.toUpperCase().includes(filter);
+    if (!this.urlBinding.loadFromParams()) this.dataSource.filterUpdated();
   }
 
   ngOnInit(): void {
-    this.dataSource = new AppDataSource<LeaveRequest>();
     this.dataSource.sort = this.matSort;
-    this.dataSource.bindToRouteData(this.route, 'leave');
   }
 
 }
