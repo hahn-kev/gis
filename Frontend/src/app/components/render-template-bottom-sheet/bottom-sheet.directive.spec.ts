a6b39274d8efb8cebe2f72c53724778483e7ff09
Kevin Hahn <hahn.kev@gmail.com>
2018-08-31 04:58:23 +0000

    bottom sheet directive: create directive for ease of use

---
+++ Frontend/src/app/components/render-template-bottom-sheet/bottom-sheet.directive.spec.ts
@@ -0,0 +1,8 @@
+import {BottomSheetDirective} from './bottom-sheet.directive';
+
+describe('BottomSheetDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BottomSheetDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ Frontend/src/app/components/render-template-bottom-sheet/bottom-sheet.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, TemplateRef} from '@angular/core';
+import {MatBottomSheet} from "@angular/material";
+
+@Directive({
+  selector: '[appBottomSheet]',
+  exportAs: 'bottomSheet'
+})
+export class BottomSheetDirective {
+  constructor(private templateRef: TemplateRef<any>, private bottomSheet: MatBottomSheet,) {
+
+  }
+
+  show(context?: any) {
+    return this.bottomSheet.open(this.templateRef, {
+      data: context
+    });
+  }
+
+}
