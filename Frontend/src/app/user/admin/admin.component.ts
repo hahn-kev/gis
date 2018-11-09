f5fe2407f8d968fbc9595b4b9e8e5318f3d9032c
Kevin Hahn <hahn.kev@gmail.com>
2018-11-09 21:29:25 +0700

    add search to admin user list

--- Frontend/src/app/user/admin/admin.component.ts
+++ Frontend/src/app/user/admin/admin.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../user';
-import { UserService } from '../user.service';
-import { AppDataSource } from '../../classes/app-data-source';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../user';
+import {UserService} from '../user.service';
+import {AppDataSource} from '../../classes/app-data-source';
+import {UrlBindingService} from '../../services/url-binding.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
+  providers: [UrlBindingService]
 })
 export class AdminComponent implements OnInit {
   dataSource: AppDataSource<User>;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private urlBinding: UrlBindingService<{ search: string }>) {
     this.dataSource = new AppDataSource<User>();
+    this.urlBinding.addParam('search', '').subscribe(value => this.dataSource.filter = value.toUpperCase());
+    this.dataSource.filterPredicate = ((data, filter) =>
+        (data.email || '').toUpperCase().startsWith(filter)
+        || (data.personName || '').toUpperCase().startsWith(filter)
+        || (data.userName || '').toUpperCase().startsWith(filter)
+    );
   }
 
   ngOnInit() {
