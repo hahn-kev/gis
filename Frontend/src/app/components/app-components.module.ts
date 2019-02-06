d847da933c259e2d475963518213cda7817a6cf3
Kevin Hahn <hahn.kev@gmail.com>
2019-02-06 22:56:06 +0700

    modules: fix lazy loading

--- Frontend/src/app/components/app-components.module.ts
+++ Frontend/src/app/components/app-components.module.ts
@@ -1,10 +1,10 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AccordionListComponent} from './accordion-list/accordion-list.component';
-import {AccordionListContentDirective} from './accordion-list/accordion-list-content.directive';
-import {AccordionListCustomActionDirective} from './accordion-list/accordion-list-custom-action.directive';
-import {AccordionListFormDirective} from './accordion-list/accordion-list-form.directive';
-import {AccordionListHeaderDirective} from './accordion-list/accordion-list-header.directive';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccordionListComponent } from './accordion-list/accordion-list.component';
+import { AccordionListContentDirective } from './accordion-list/accordion-list-content.directive';
+import { AccordionListCustomActionDirective } from './accordion-list/accordion-list-custom-action.directive';
+import { AccordionListFormDirective } from './accordion-list/accordion-list-form.directive';
+import { AccordionListHeaderDirective } from './accordion-list/accordion-list-header.directive';
 import {
   MatAutocompleteModule,
   MatBadgeModule,
@@ -34,23 +34,24 @@ import {
   MatTooltipModule,
   MatTreeModule
 } from '@angular/material';
-import {MatMomentDateModule} from '@angular/material-moment-adapter';
-import {ObserversModule} from '@angular/cdk/observers';
-import {FormsModule} from '@angular/forms';
-import {IsUserPolicyPipe} from '../services/auth/is-user-policy.pipe';
-import {AppTemplateContentDirective} from '../directives/app-template-content.directive';
-import {ExportButtonComponent} from './export-button/export-button.component';
-import {RenderTemplateDialogComponent} from './render-template-dialog/render-template-dialog.component';
-import {TitleCasePipe} from '../services/title-case.pipe';
-import {DialogDirective} from './render-template-dialog/dialog.directive';
-import {QuickAddComponent} from '../people/person/quick-add/quick-add.component';
-import {QuickAddDirective} from '../people/person/quick-add/quick-add.directive';
-import {QuickAddButtonComponent} from '../people/person/quick-add/quick-add-button.component';
-import {RoleComponent} from '../people/person/role.component';
-import {CookieService} from 'ngx-cookie-service';
-import {RouterModule} from '@angular/router';
-import {ToolbarContentDirective} from '../toolbar/toolbar-content.directive';
-import {ToolbarTemplateDirective} from '../toolbar/toolbar-template.directive';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { ObserversModule } from '@angular/cdk/observers';
+import { FormsModule } from '@angular/forms';
+import { IsUserPolicyPipe } from '../services/auth/is-user-policy.pipe';
+import { AppTemplateContentDirective } from '../directives/app-template-content.directive';
+import { ExportButtonComponent } from './export-button/export-button.component';
+import { RenderTemplateDialogComponent } from './render-template-dialog/render-template-dialog.component';
+import { TitleCasePipe } from '../services/title-case.pipe';
+import { DialogDirective } from './render-template-dialog/dialog.directive';
+import { QuickAddComponent } from '../people/person/quick-add/quick-add.component';
+import { QuickAddDirective } from '../people/person/quick-add/quick-add.directive';
+import { QuickAddButtonComponent } from '../people/person/quick-add/quick-add-button.component';
+import { RoleComponent } from '../people/person/role.component';
+import { CookieService } from 'ngx-cookie-service';
+import { RouterModule } from '@angular/router';
+import { ToolbarContentDirective } from '../toolbar/toolbar-content.directive';
+import { ToolbarTemplateDirective } from '../toolbar/toolbar-template.directive';
+import { FindFormPipe } from './accordion-list/find-form.pipe';
 
 const matModules = [
   MatButtonModule,
@@ -93,6 +94,7 @@ const matModules = [
     AccordionListFormDirective,
     AccordionListHeaderDirective,
     AccordionListCustomActionDirective,
+    FindFormPipe,
     IsUserPolicyPipe,
     AppTemplateContentDirective,
     ExportButtonComponent,
