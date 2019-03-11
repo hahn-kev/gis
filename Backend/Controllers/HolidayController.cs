36ad140db84f7df16092c8ef01172917ca660d9d
Kevin Hahn <hahn.kev@gmail.com>
2019-03-11 12:24:33 +0700

    holidays: add CRUD methods

--- Backend/Controllers/HolidayController.cs
+++ Backend/Controllers/HolidayController.cs
@@ -1,16 +1,56 @@
 using System;
 using System.Collections.Generic;
+using Backend.Controllers;
+using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/[controller]")]
-public class HolidayController
+public class HolidayController : MyController
 {
+    private readonly IEntityService _entityService;
+
+    public HolidayController(IEntityService entityService)
+    {
+        _entityService = entityService;
+    }
+
+
+
     [HttpGet]
-    public List<Holiday> Holidays() {
-        return new List<Holiday> {
-            new Holiday {Name = "Christmas", Start = new DateTime(2019, 12, 24), End = new DateTime(2019, 12, 30)},
-            new Holiday {Name = "New Years Day", Start = new DateTime(2019, 1, 1), End = new DateTime(2019, 1, 1)},
-            new Holiday {Name = "Songkran", Start = new DateTime(2019, 4, 16), End = new DateTime(2019, 4, 17)},
-        };
+    public List<Holiday> Holidays()
+    {
+        return _entityService.List<Holiday>();
+    }
+
+    [HttpGet("current")]
+    public List<Holiday> CurrentHolidays()
+    {
+        //todo filter by date
+        return _entityService.List<Holiday>();
+    }
+
+
+
+    [HttpGet("{id}")]
+    [Authorize(Policy = "jobs")]
+    public Holiday GetById(Guid id)
+    {
+        return _entityService.GetById<Holiday>(id);
+    }
+
+    [HttpPost]
+    [Authorize("holidays")]
+    public Holiday Save([FromBody] Holiday holiday)
+    {
+        _entityService.Save(holiday);
+        return holiday;
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid id)
+    {
+        _entityService.Delete<Holiday>(id);
+        return Ok();
     }
 }
