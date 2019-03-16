3c42e3bdf23b126cc495f996813a5b5518813df0
Kevin Hahn <hahn.kev@gmail.com>
2019-03-16 12:36:46 +0700

    org group type name, inline method into pipe

--- Frontend/src/app/people/groups/group-type-name.pipe.ts
+++ Frontend/src/app/people/groups/group-type-name.pipe.ts
@@ -1,13 +1,18 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {GroupType, GroupTypeName} from './org-group';
+import { Pipe, PipeTransform } from '@angular/core';
+import { GroupType } from './org-group';
 
 @Pipe({
   name: 'groupTypeName'
 })
 export class GroupTypeNamePipe implements PipeTransform {
 
-  transform(value: GroupType): any {
-    return GroupTypeName(value);
+  transform(groupType: GroupType): string {
+    switch (groupType) {
+      case GroupType.Supervisor:
+        return 'Office';
+      default:
+        return groupType;
+    }
   }
 
 }
